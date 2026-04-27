388bbeae8108150bd7ddcff6246bd95481ef04b7
Urvino <78988375+vcastelan@users.noreply.github.com>
2026-04-27 12:45:21 -0700

    Deleted uneccessary code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import './App.css'
 function App() {
  
   return (
-    <div>
-      <h1>My Todos</h1>
+    
       <div>
         <h1>Todo List</h1>
         <TodoForm />
         <TodoList />
       </div>
       
-    </div>
   )
 }
 
